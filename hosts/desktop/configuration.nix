51460262ac4b4b54b4e090a50ae1247dac3bc02f
station <987654321mai6@gmail.com>
2025-07-23 12:17:43 +0200

    25   2025-07-23 10:24:45   (current)

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -63,6 +63,7 @@ in
   };
   
 	hardware.nvidia.prime = {
+	  sync.enable = true;
 		# Make sure to use the correct Bus ID values for your system!
 		intelBusId = "PCI:00:02:0";
 		nvidiaBusId = "PCI:01:00:0";
